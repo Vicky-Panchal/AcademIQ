0614c5247e2717d07b19f03227f38dfb1b94c65b
Vicky Panchal <vmp9905@gmail.com>
2023-11-18 09:42:24 +0530

    Springboot boilerplate

---
+++ src/main/java/com/academiq/academiq/AcademiqApplication.java
@@ -0,0 +1,13 @@
+package com.academiq.academiq;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AcademiqApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AcademiqApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/academiq/academiq/AcademiqApplicationTests.java
@@ -0,0 +1,13 @@
+package com.academiq.academiq;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AcademiqApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
